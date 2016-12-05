3299d3ec8d37857d3ffb90b7f1bb10a2e18a5544
Gili Tzabari <cowwoc2020@gmail.com>
2016-12-04 22:16:08 -0500

    ArrayRequirementsImpl.isIn() was invoking the wrong toString() method; fixed. Upgraded CMake version. Util.h was missing includes under Windows.

--- native/src/main/cpp/include/requirements/Util.h
+++ native/src/main/cpp/include/requirements/Util.h
@@ -5,6 +5,22 @@
 
 #include <jni.h>
 
+#ifndef VC_EXTRALEAN
+#define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
+#endif
+
+// Modify the following defines if you have to target a platform prior to the ones specified below.
+// Refer to MSDN for the latest info on corresponding values for different platforms.
+#ifndef WINVER
+#define WINVER 0x0A00
+#endif
+
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0A00
+#endif
+
+#include <windows.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
