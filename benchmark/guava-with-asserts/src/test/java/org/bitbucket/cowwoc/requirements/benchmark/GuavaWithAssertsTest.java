04d1608f44098a271f57dd98d3c118bda21ea30e
Gili Tzabari <cowwoc2020@gmail.com>
2017-07-28 15:05:35 -0400

    Moved most benchmark methods from guava-with-asserts to guava-without-asserts under the assumption that this represents the typical usage.

--- benchmark/guava-with-asserts/src/test/java/org/bitbucket/cowwoc/requirements/benchmark/GuavaWithAssertsTest.java
+++ benchmark/guava-with-asserts/src/test/java/org/bitbucket/cowwoc/requirements/benchmark/GuavaWithAssertsTest.java
@@ -6,20 +6,14 @@ package org.bitbucket.cowwoc.requirements.benchmark;
 
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
-import org.bitbucket.cowwoc.requirements.core.CollectionVerifier;
-import org.bitbucket.cowwoc.requirements.core.Verifiers;
 import org.bitbucket.cowwoc.requirements.core.terminal.TerminalEncoding;
 import org.bitbucket.cowwoc.requirements.guava.MultimapVerifier;
 import static org.bitbucket.cowwoc.requirements.guava.Requirements.assertThat;
-import static org.bitbucket.cowwoc.requirements.guava.Requirements.requireThat;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.infra.Blackhole;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
@@ -33,15 +27,9 @@ public class GuavaWithAssertsTest
 {
 	private String name = "name";
 	private Multimap<String, String> value = HashMultimap.create();
-	private Multimap<?, ?> nullMultimap = null;
-	private List<Integer> list;
 
 	public GuavaWithAssertsTest()
 	{
-		value.put("key", "value");
-		list = new ArrayList<>(100);
-		for (int i = 0; i < 100; ++i)
-			list.add(i);
 	}
 
 	@Test
@@ -64,72 +52,5 @@ public class GuavaWithAssertsTest
 	public MultimapVerifier<?, ?> staticAssertThat()
 	{
 		return assertThat(name, value).isNotNull().isNotEmpty();
-	}
-
-	@Benchmark
-	public MultimapVerifier<?, ?> verifiersAssertsDisabled()
-	{
-		return new Verifiers().withAssertionsDisabled().assertThat(name, value).isNotNull().isNotEmpty();
-	}
-
-	// See http://stackoverflow.com/a/38862964/14731 for why assertThat() can
-	// be faster than requireThat() even though it delegates to it
-	@Benchmark
-	public MultimapVerifier<?, ?> verifiersAssertsEnabled()
-	{
-		return new Verifiers().withAssertionsEnabled().assertThat(name, value).isNotNull().isNotEmpty();
-	}
-
-	// See http://stackoverflow.com/a/38862964/14731 for why assertThat() can
-	// be faster than requireThat() even though it delegates to it
-	@Benchmark
-	public MultimapVerifier<?, ?> verifiersRequireThat()
-	{
-		return new Verifiers().requireThat(name, value).isNotNull().isNotEmpty();
-	}
-
-	@Benchmark
-	public MultimapVerifier<?, ?> staticRequireThat()
-	{
-		return requireThat(name, value).isNotNull().isNotEmpty();
-	}
-
-	@Benchmark
-	public CollectionVerifier<?, ?> verifiersRequireThatDoesNotContainDuplicates()
-	{
-		return new Verifiers().requireThat(name, list).doesNotContainDuplicates();
-	}
-
-	@Benchmark
-	public CollectionVerifier<?, ?> verifiersAssertsDisabledDoesNotContainDuplicates()
-	{
-		return new Verifiers().withAssertionsDisabled().assertThat(name, list).
-			doesNotContainDuplicates();
-	}
-
-	@Benchmark
-	public void staticThrowException(Blackhole bh)
-	{
-		try
-		{
-			requireThat(name, nullMultimap).isNotNull();
-		}
-		catch (NullPointerException e)
-		{
-			bh.consume(e);
-		}
-	}
-
-	@Benchmark
-	public void verifiersThrowException(Blackhole bh)
-	{
-		try
-		{
-			new Verifiers().requireThat(name, nullMultimap).isNotNull();
-		}
-		catch (NullPointerException e)
-		{
-			bh.consume(e);
-		}
 	}
 }
